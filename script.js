7b0e5331c0f26717f3453749ba349e8f095e3e90
Miro-png <58429869+Miro-png@users.noreply.github.com>
2019-12-03 17:10:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+   <SCRIPT>
+function passWord() {
+var testV = 1;
+var pass1 = prompt('Please Enter Your Password',' ');
+while (testV < 3) {
+if (!pass1) 
+history.go(-1);
+if (pass1.toLowerCase() == "coding") {
+alert('You Got it Right!');
+window.open('https://competent-gates-7f6f9f.netlify.com/');
+break;
+} 
+testV+=1;
+var pass1 = 
+prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
+}
+if (pass1.toLowerCase()!="password" & testV ==3) 
+history.go(-1);
+return " ";
+} 
+</SCRIPT>
+<CENTER>
+<FORM>
    passWord();
      //this prevents the program from moving on until the correct
   //password has been entered
