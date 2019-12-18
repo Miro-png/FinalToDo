ec07cb6d533d44b63f460b9626b6e73543dbeb39
Miro-png <58429869+Miro-png@users.noreply.github.com>
2019-12-18 14:25:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){function passWord(){var testV=1;var pass1=prompt('Please Enter Your Password',' ');while(testV<3){if(!pass1)
+history.go(-1);if(pass1.toLowerCase()=="coding"){alert('You Got it Right!');window.open('https://competent-gates-7f6f9f.netlify.com/');break}
+testV+=1;var pass1=prompt('Access Denied - Password Incorrect, Please Try Again.','Password')}
+if(pass1.toLowerCase()!="password"&testV==3)
+history.go(-1);return" "}
+passWord();$("#list-items").html(localStorage.getItem("listItems"));$(".add-items").submit(function(event){event.preventDefault();var item=$("#todo-list-item").val();if(item){$("#list-items").append("<li><input class='checkbox' type='checkbox' />"+item+" <a class='remove'>Delete</a><hr></li>");localStorage.setItem("listItems",$("#list-items").html());$("#todo-list-item").val("")}else alert("Write something dummy")});$(document).on("change",".checkbox",function(){if($(this).attr("checked")){$(this).removeAttr("checked")}else{$(this).attr("checked","checked")}
+$(this).parent().toggleClass("completed");localStorage.setItem("listItem",$("#list-items").html())});$(document).on("click",".remove",function(){$(this).parent().remove();localStorage.setItem("listItem",$("#list-items").html())})})
+
 anime.timeline({loop: true})
   .add({
     targets: '.ml15 .word',
@@ -13,11 +21,3 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
-
-$(document).ready(function(){function passWord(){var testV=1;var pass1=prompt('Please Enter Your Password',' ');while(testV<3){if(!pass1)
-history.go(-1);if(pass1.toLowerCase()=="coding"){alert('You Got it Right!');window.open('https://competent-gates-7f6f9f.netlify.com/');break}
-testV+=1;var pass1=prompt('Access Denied - Password Incorrect, Please Try Again.','Password')}
-if(pass1.toLowerCase()!="password"&testV==3)
-history.go(-1);return" "}
-passWord();$("#list-items").html(localStorage.getItem("listItems"));$(".add-items").submit(function(event){event.preventDefault();var item=$("#todo-list-item").val();if(item){$("#list-items").append("<li><input class='checkbox' type='checkbox' />"+item+" <a class='remove'>Delete</a><hr></li>");localStorage.setItem("listItems",$("#list-items").html());$("#todo-list-item").val("")}else alert("Write something dummy")});$(document).on("change",".checkbox",function(){if($(this).attr("checked")){$(this).removeAttr("checked")}else{$(this).attr("checked","checked")}
-$(this).parent().toggleClass("completed");localStorage.setItem("listItem",$("#list-items").html())});$(document).on("click",".remove",function(){$(this).parent().remove();localStorage.setItem("listItem",$("#list-items").html())})})
